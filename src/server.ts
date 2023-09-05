1a4d686138e580bd89a1d224eb2ea32d93299d55
luckywebdevloper <mobiartlucky@gmail.com>
2023-09-05 11:29:25 +0530

    env file added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from "express";
 require("dotenv").config();
+import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
 import { swaggerDocOptions } from "./swaggerDocOptions";
